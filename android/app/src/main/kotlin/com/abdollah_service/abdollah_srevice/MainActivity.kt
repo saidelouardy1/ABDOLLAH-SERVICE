9a951c19677074f62b6082f3596bb40c43b5c0bb
said <saidelouardy1.saidelouardy1>
2025-01-07 10:45:40 +0100

    create cline app

---
+++ android/app/src/main/kotlin/com/abdollah_service/abdollah_srevice/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.abdollah_service.abdollah_srevice
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/app/routes/app_pages.dart
@@ -0,0 +1,34 @@
+import 'package:get/get.dart';
+
+import '../modules/Authentication/bindings/authentication_binding.dart';
+import '../modules/Authentication/views/authentication_view.dart';
+import '../modules/SplashScreen/bindings/splash_screen_binding.dart';
+import '../modules/SplashScreen/views/splash_screen_view.dart';
+import '../modules/home/bindings/home_binding.dart';
+import '../modules/home/views/home_view.dart';
+
+part 'app_routes.dart';
+
+class AppPages {
+  AppPages._();
+
+  static const INITIAL = Routes.SPLASH_SCREEN;
+
+  static final routes = [
+    GetPage(
+      name: _Paths.HOME,
+      page: () => const HomeView(),
+      binding: HomeBinding(),
+    ),
+    GetPage(
+      name: _Paths.SPLASH_SCREEN,
+      page: () => const SplashScreenView(),
+      binding: SplashScreenBinding(),
+    ),
+    GetPage(
+      name: _Paths.AUTHENTICATION,
+      page: () => const AuthenticationView(),
+      binding: AuthenticationBinding(),
+    ),
+  ];
+}
